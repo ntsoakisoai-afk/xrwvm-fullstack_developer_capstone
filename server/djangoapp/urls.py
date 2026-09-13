c4323aa482cb77ae82cb753c406b685986de2ce9
ntsoakisoai-afk <ntsoakisoai@gmail.com>
2026-09-13 15:35:13 -0400

    Added and Registered car models

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -2,17 +2,11 @@
 # from django.urls import path
 from django.conf.urls.static import static
 from django.conf import settings
+from django.urls import path
+from . import views
 # from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # # path for registration
-
-    # path for login
-    # path(route='login', view=views.login_user, name='login'),
-
-    # path for dealer reviews view
-
-    # path for add a review view
-
+    path(route='get_cars', view=views.get_cars, name='getcars'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- server/djangoapp/views.py
+++ server/djangoapp/views.py
@@ -1,65 +1,126 @@
-# Uncomment the required imports before adding the code
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect, HttpResponse, JsonResponse
+from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate, logout
+from django.contrib import messages
+from datetime import datetime
+from django.views.decorators.csrf import csrf_exempt
+from .models import CarMake, CarModel
+from .populate import initiate
 
-# from django.shortcuts import render
-# from django.http import HttpResponseRedirect, HttpResponse
-# from django.contrib.auth.models import User
-# from django.shortcuts import get_object_or_404, render, redirect
-# from django.contrib.auth import logout
-# from django.contrib import messages
-# from datetime import datetime
 
-from django.http import JsonResponse
-from django.contrib.auth import login, authenticate
 import logging
 import json
-from django.views.decorators.csrf import csrf_exempt
-# from .populate import initiate
 
-
-# Get an instance of a logger
 logger = logging.getLogger(__name__)
 
 
-# Create your views here.
-
-# Create a `login_request` view to handle sign in request
 @csrf_exempt
 def login_user(request):
-    # Get username and password from request.POST dictionary
     data = json.loads(request.body)
     username = data['userName']
     password = data['password']
-    # Try to check if provide credential can be authenticated
+
     user = authenticate(username=username, password=password)
+
     data = {"userName": username}
+
     if user is not None:
-        # If user is valid, call login method to login current user
         login(request, user)
-        data = {"userName": username, "status": "Authenticated"}
+        data = {
+            "userName": username,
+            "status": "Authenticated"
+        }
+
     return JsonResponse(data)
 
-# Create a `logout_request` view to handle sign out request
-# def logout_request(request):
-# ...
 
-# Create a `registration` view to handle sign up request
-# @csrf_exempt
-# def registration(request):
-# ...
+# logout request
+def logout_request(request):
+    logout(request)
+    return HttpResponseRedirect("/")
 
-# # Update the `get_dealerships` view to render the index page with
-# a list of dealerships
-# def get_dealerships(request):
-# ...
 
-# Create a `get_dealer_reviews` view to render the reviews of a dealer
-# def get_dealer_reviews(request,dealer_id):
-# ...
+# registration request
+@csrf_exempt
+def registration(request):
 
-# Create a `get_dealer_details` view to render the dealer details
-# def get_dealer_details(request, dealer_id):
-# ...
+    data = json.loads(request.body)
 
-# Create a `add_review` view to submit a review
-# def add_review(request):
-# ...
+    username = data['userName']
+    password = data['password']
+    first_name = data['firstName']
+    last_name = data['lastName']
+    email = data['email']
+
+    username_exists = User.objects.filter(username=username).exists()
+
+    if username_exists:
+        return JsonResponse({
+            "userName": username,
+            "error": "Already Registered"
+        })
+
+    user = User.objects.create_user(
+        username=username,
+        password=password,
+        email=email,
+        first_name=first_name,
+        last_name=last_name
+    )
+
+    login(request, user)
+
+    return JsonResponse({
+        "userName": username,
+        "status": "Authenticated"
+    })
+
+
+# dealership list
+def get_dealerships(request):
+    return render(request, "Home.html")
+
+
+# dealer reviews
+def get_dealer_reviews(request, dealer_id):
+    return render(
+        request,
+        "Home.html",
+        {"dealer_id": dealer_id}
+    )
+
+
+# dealer details
+def get_dealer_details(request, dealer_id):
+    return JsonResponse(
+        {"dealer_id": dealer_id}
+    )
+
+
+# add review
+def add_review(request):
+    return render(request, "Home.html")
+
+
+# get cars
+def get_cars(request):
+
+    count = CarMake.objects.filter().count()
+
+    print(count)
+
+    if count == 0:
+        initiate()
+
+    car_models = CarModel.objects.select_related('car_make')
+
+    cars = []
+
+    for car_model in car_models:
+        cars.append({
+            "CarModel": car_model.name,
+            "CarMake": car_model.car_make.name
+        })
+
+    return JsonResponse({"CarModels": cars})
