0c4dd09ec77c824f60e39e9e4b55f4e9355588e4
ntsoakisoai-afk <ntsoakisoai@gmail.com>
2026-09-14 09:46:34 -0400

    Changes

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -1,3 +1,4 @@
+import PostReview from "./components/Dealers/PostReview";
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom";
 import Dealers from './components/Dealers/Dealers';
@@ -10,7 +11,7 @@ function App() {
         <Route path="/login" element={<LoginPanel />} />
         <Route path="/dealers" element={<Dealers/>} />
     	<Route path="/dealer/:id" element={<Dealer/>} />
-
+        <Route path="/postreview/:id" element={<PostReview/>} />
     </Routes>
   );
 }
