d2cbaddc673901bfd1c7599955175ec0abd2da51
ntsoakisoai-afk <ntsoakisoai@gmail.com>
2026-09-23 16:01:37 -0400

    Fixed JS linting errors

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,33 +1,40 @@
 # Uncomment the imports below before you add the function code
-import requests
 import os
 from urllib.parse import quote
+
+import requests
 from dotenv import load_dotenv
+
 
 load_dotenv()
 
 backend_url = os.getenv(
-    'backend_url', default="http://localhost:3030")
+    "backend_url",
+    default="http://localhost:3030"
+)
 sentiment_analyzer_url = os.getenv(
-    'sentiment_analyzer_url',
-    default="http://localhost:5050/")
+    "sentiment_analyzer_url",
+    default="http://localhost:5050/"
+)
+
 
 def get_request(endpoint, **kwargs):
     params = ""
-    if(kwargs):
-        for key,value in kwargs.items():
-            params=params+key+"="+value+"&"
+    if kwargs:
+        for key, value in kwargs.items():
+            params = params + key + "=" + value + "&"
 
-    request_url = backend_url+endpoint+"?"+params
+    request_url = backend_url + endpoint + "?" + params
 
     print("GET from {} ".format(request_url))
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except:
+    except Exception:
         # If any error occurs
         print("Network exception occurred")
+
 
 def analyze_review_sentiments(text):
     try:
@@ -42,13 +49,15 @@ def analyze_review_sentiments(text):
         print("Network exception occurred")
         return None
 
+
 def post_review(data_dict):
-    request_url = backend_url+"/insert_review"
+    request_url = backend_url + "/insert_review"
     try:
-        response = requests.post(request_url,json=data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
-    except:
+    except Exception:
         print("Network exception occurred")
+
 
 # Add code for posting review
--- server/djangoproj/settings.py
+++ server/djangoproj/settings.py
@@ -28,8 +28,21 @@ SECRET_KEY =\
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1", "ntsoakisoai-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"]
-CSRF_TRUSTED_ORIGINS = ["https://ntsoakisoai-8000.theiadockernext-1-labs-prod-theiak8s-4-tor01.proxy.cognitiveclass.ai"]
+ALLOWED_HOSTS = [
+    "localhost",
+    "127.0.0.1",
+    (
+        "ntsoakisoai-8000.theiadockernext-1-labs-prod-"
+        "theiak8s-4-tor01.proxy.cognitiveclass.ai"
+    ),
+]
+
+CSRF_TRUSTED_ORIGINS = [
+    (
+        "https://ntsoakisoai-8000.theiadockernext-1-labs-prod-"
+        "theiak8s-4-tor01.proxy.cognitiveclass.ai"
+    ),
+]
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': [],
@@ -93,9 +106,12 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME':
-        'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': (
+            'django.contrib.auth.password_validation.'
+            'UserAttributeSimilarityValidator'
+        ),
     },
+
     {
         'NAME':
         'django.contrib.auth.password_validation.MinimumLengthValidator',
@@ -140,7 +156,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'frontend/static'),
-	os.path.join(BASE_DIR, 'frontend/build'),
-	os.path.join(BASE_DIR, 'frontend/build/static'),
+    os.path.join(BASE_DIR, 'frontend/build'),
+    os.path.join(BASE_DIR, 'frontend/build/static'),
 ]
-
